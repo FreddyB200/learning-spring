7bb9152757fab71647f5cf94ff3765549f3f3eae
Freddy Bautista <abs0rbmejohan3212@gmail.com>
2025-04-19 03:25:26 -0500

    docs: adding UserDTO notes

--- springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/dto/UserDTO.java
+++ springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/dto/UserDTO.java
@@ -1,11 +1,19 @@
 package com.freddy.curso.springboot.webapp.springboot_web.dto;
-
-import com.freddy.curso.springboot.webapp.springboot_web.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
+
+/**
+ * DTO para exponer solo los datos necesarios del usuario en la API.
+ *
+ * Notas de aprendizaje:
+ * - Se utiliza para ocultar información sensible o innecesaria de la entidad User.
+ * - Incluye solo id, nombre completo y email.
+ * - Se construye a partir de la entidad User usando UserMapper.
+ * - Usa Lombok para reducir el código repetitivo.
+ */
+
 
 @Data
 @NoArgsConstructor
