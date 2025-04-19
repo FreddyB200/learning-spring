882ff2b50c0bdc042f1df00cfd0ca63388995dd1
Freddy Bautista <abs0rbmejohan3212@gmail.com>
2025-04-19 03:24:00 -0500

    docs: adding ParamDTO notes

--- springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/dto/ParamDTO.java
+++ springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/dto/ParamDTO.java
@@ -3,6 +3,15 @@ package com.freddy.curso.springboot.webapp.springboot_web.dto;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * DTO (Data Transfer Object) para transportar parámetros recibidos en peticiones HTTP.
+ *
+ * Notas de aprendizaje:
+ * - Se utiliza para devolver datos simples (como parámetros de consulta o path variables) en los controladores REST.
+ * - Incluye los campos 'message' (texto) y 'code' (número).
+ * - Usa Lombok para generar getters, setters y el patrón builder.
+ */
+
 @Data
 @Builder
 public class ParamDTO {
