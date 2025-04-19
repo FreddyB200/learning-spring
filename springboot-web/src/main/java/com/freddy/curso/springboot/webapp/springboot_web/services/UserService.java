0c978be118350635aebe59f43b4cc8f8e58107e3
Freddy Bautista <abs0rbmejohan3212@gmail.com>
2025-04-19 03:25:09 -0500

    docs: adding User service notes

--- springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/services/UserService.java
+++ springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/services/UserService.java
@@ -1,12 +1,18 @@
 package com.freddy.curso.springboot.webapp.springboot_web.services;
-
 import com.freddy.curso.springboot.webapp.springboot_web.models.User;
 import com.freddy.curso.springboot.webapp.springboot_web.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
+/**
+ * Servicio para la lógica de negocio relacionada con usuarios.
+ *
+ * Notas de aprendizaje:
+ * - Se encarga de obtener los usuarios desde el repositorio.
+ * - Aquí se puede agregar lógica adicional antes de devolver los datos al controlador.
+ * - Es el punto de acceso para los controladores que necesitan información de usuarios.
+ */
 
 @AllArgsConstructor //inyeccion por constructor //no usarlo con mas de 2 constructores
 @Service
