8794594d7bcbc002a143d02d4041c1657f317169
Freddy Bautista <abs0rbmejohan3212@gmail.com>
2025-04-19 03:24:31 -0500

    docs: adding User entity notes

--- springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/models/User.java
+++ springboot-web/src/main/java/com/freddy/curso/springboot/webapp/springboot_web/models/User.java
@@ -1,8 +1,17 @@
 package com.freddy.curso.springboot.webapp.springboot_web.models;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+/**
+ * Entidad User que representa un usuario del sistema.
+ *
+ * Notas de aprendizaje:
+ * - Es la clase principal para manejar usuarios en la aplicación.
+ * - Incluye id, nombre, apellido, email y rol.
+ * - Se utiliza tanto en la lógica interna como para exponer datos completos (solo en endpoints que lo requieran).
+ * - Usa Lombok para generar constructores y métodos estándar.
+ */
 
 @Data
 @NoArgsConstructor
